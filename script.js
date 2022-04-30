3e2840e10caf13a10a8cd64ae6a31feddff199e9
Daphne Castro <dc4768@nyu.edu>
2022-04-30 15:47:15 -0400

    j

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 $( function() {
    $( ".draggable" ).draggable();
  } );
+
+var button_1;
+var check;
+var visibility_;
+
+document.addEventListener("DOMContentLoaded", function() {
+  button_1 = document.getElementById("button1");
+  check = 0;
+  visibility_ = document.getElementById("visibility");
+
+  button_1.addEventListener("onClick", function(){
+    check = 1;
+    displayHistory(check);
+  })
+});
+
+function displayHistory(check) {
+    if (check == 1) {
+      visibility: visible;
+    }
+
+}
