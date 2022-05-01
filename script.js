c67eb88c6783ca12f84b3e95c731400efa0c8bf2
Daphne Castro <dc4768@nyu.edu>
2022-05-01 14:40:48 -0400

    d

--- script.js
+++ script.js
@@ -1,24 +1,37 @@
 $( function() {
-   $( ".draggable" ).draggable();
+   $( ".draggable" ).draggable({
+     stack: "div" /* Option to let the windows stack and pop over each other correctly */
+   });
  } );
 
 var button_1;
-var check;
-var visibility_;
+var ukraineButton;
+var russiaButton;
+var historyPopup;
+var historyPopup1;
+var ukrainePopup;
+var russiaPopup;
 
 document.addEventListener("DOMContentLoaded", function() {
   button_1 = document.getElementById("button1");
-  check = 0;
-  visibility_ = document.getElementById("visibility");
 
+  ukraineButton = document.getElementById("ukraineButton");
+  russiaButton = document.getElementById("russiaButton");
+  historyPopup = document.getElementById("historyPopup");
+  historyPopup1 = document.getElementById("historyPopup1");
+  ukrainePopup = document.getElementById("ukrainePopup");
+  russiaPopup = document.getElementById("russiaPopup");
   button_1.addEventListener("click", function(){
-    check = 1;
-    displayHistory(check);
+    historyPopup.style.display = "block";
+  })
+  button_1.addEventListener("click", function(){
+    historyPopup.style.display = "block";
+    historyPopup1.style.display = "block";
+  })
+  ukraineButton.addEventListener("click", function(){
+    ukrainePopup.style.display = "block";
+  })
+  russiaButton.addEventListener("click", function(){
+    russiaPopup.style.display = "block";
   })
 });
-
-function displayHistory(check) {
-    if (check == 1) {
-      visibility: visible;
-    }
-}
