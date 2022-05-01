3de2f06babfb9f731f7c06394cc91dff9e471935
Daphne Castro <dc4768@nyu.edu>
2022-05-01 16:05:48 -0400

    k

--- script.js
+++ script.js
@@ -12,7 +12,10 @@ var historyPopup1;
 var historyPopup2;
 var historyPopup3;
 var ukrainePopup;
+var ukrainePopup1;
 var russiaPopup;
+var russiaPopup1;
+var russiaPopup2;
 
 document.addEventListener("DOMContentLoaded", function() {
   button_1 = document.getElementById("button1");
@@ -23,7 +26,10 @@ document.addEventListener("DOMContentLoaded", function() {
   historyPopup2 = document.getElementById("historyPopup2");
   historyPopup3 = document.getElementById("historyPopup3");
   ukrainePopup = document.getElementById("ukrainePopup");
+  ukrainePopup1 = document.getElementById("ukrainePopup1");
   russiaPopup = document.getElementById("russiaPopup");
+  russiaPopup1 = document.getElementById("russiaPopup1");
+  russiaPopup2 = document.getElementById("russiaPopup2");
   button_1.addEventListener("click", function(){
     historyPopup.style.display = "block";
   })
@@ -35,8 +41,11 @@ document.addEventListener("DOMContentLoaded", function() {
   })
   ukraineButton.addEventListener("click", function(){
     ukrainePopup.style.display = "block";
+    ukrainePopup1.style.display = "block";
   })
   russiaButton.addEventListener("click", function(){
     russiaPopup.style.display = "block";
+    russiaPopup1.style.display = "block";
+    russiaPopup2.style.display = "block";
   })
 });
